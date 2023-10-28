e7ce84b21cc4d6d55b5261d283121701f1e8d563
RajaniVucha <128374452+RajaniVucha@users.noreply.github.com>
2023-10-28 18:43:38 +0530

    Update addStudent.jsx

--- client/src/components/addStudent.jsx
+++ client/src/components/addStudent.jsx
@@ -72,7 +72,7 @@ const AddStudent = () => {
 
       console.log(student);
       // Make a POST request to your API endpoint
-      fetch("http://localhost:3000/students", {
+      fetch("https://schoolcool-two.vercel.app/students", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
